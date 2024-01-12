1dce2bf32859db49248b285979f1fc79049c8b81
saranatour1 <saranatourcontact@gmail.com>
2024-01-12 18:56:00 +0200

    update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'red':{
+          'tomato':"hsl(4, 100%, 67%)",
+          'tomato-1':"#FF5474",
+          "tomato-2":"#FF6740"
+        },
+        'gray':{
+          '960':'hsl(231, 7%, 60%)',
+          '970':'hsl(235, 18%, 26%)',
+          '1000':"hsl(234, 29%, 20%)",
+        }
+      },
+      gradientColorStops:{},
+      fontFamily:{
+        'roboto':['Roboto', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
