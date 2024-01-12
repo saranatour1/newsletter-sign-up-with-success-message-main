4fc38833356fbc35bf72bae6d8d01b260353d6b9
saranatour1 <saranatourcontact@gmail.com>
2024-01-12 18:56:15 +0200

    update font

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import NewsLetter from './components/NewsLetter.vue';
-
-
+  import NewsLetter from './components/NewsLetter.vue';
 </script>
 
 <template>
   <NewsLetter />
 </template>
-
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-
-</style>
